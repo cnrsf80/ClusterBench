f122e12b9571b0e880fb8075b4567296af310b59
hhoareau <hhoareau@gmail.com>
2018-10-16 19:28:21 +0200

    Mise en place du serveur web

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import clusterBench.simulation as simulation
 import clusterBench.algo as algo
 import pandas as pd
 import main
+import copy
 import clusterBench.tools as tools
 from sklearn import cluster as cl
 import hdbscan
@@ -26,11 +27,12 @@ def index():
     code=code+tools.addlink("http://127.0.0.1:5000/algo/hac/http%3A%2F%2Ff80.fr%2Fcnrs%2Fdatas%2FPourClustering.csv/n_clusters=12,11,13")
     code=code+tools.addlink("http://127.0.0.1:5000/algo/meanshift/http%3A%2F%2Ff80.fr%2Fcnrs%2Fdatas%2FPourClustering.csv/bandwidth=0.1,0.3,0.2")
     code=code+tools.addlink("http://127.0.0.1:5000/algo/hdbscan/http%3A%2F%2Ff80.fr%2Fcnrs%2Fdatas%2FPourClustering.csv/min_cluster_size=5,6,7,8&alpha=0.1,0.3,0.5,0.9")
+    code=code+tools.addlink("http://127.0.0.1:5000/algo/neuralgas/http%3A%2F%2Ff80.fr%2Fcnrs%2Fdatas%2FPourClustering.csv/passes=10,20,60,120&distance_toremove_edge=40")
     return code
 
 
 @app.route('/algo/<string:name_algo>/<path:url>/<string:params>', methods=['GET'])
-def algo(url:str,params:str,name_algo:str):
+def algo_func(url:str,params:str,name_algo:str):
     data=None
     if url.endswith(".xlsx"):data=pd.read_excel(url)
     if url.endswith(".csv"):data=pd.read_csv(url,sep=";")
@@ -69,6 +71,19 @@ def algo(url:str,params:str,name_algo:str):
                     cl.AgglomerativeClustering(n_clusters=x["n_clusters"]),
                   parameters
                   )
+
+    if name_algo.upper().__contains__("NEURALGAS"):
+        parameters = tools.buildDict(params, {"passes": [50],"distance_toremove":[60]})
+        for passes in parameters.get("passes"):
+            for distance_toremove_edge in parameters.get("distance_toremove_edge"):
+                m:algo.model = algo.create_cluster_from_neuralgasnetwork(
+                    copy.deepcopy(sim.ref_model).clear_clusters(),
+                    a=0.5,
+                    passes=passes,
+                    distance_toremove_edge=distance_toremove_edge)
+                m.params = [passes, distance_toremove_edge, ""]
+                m.help = "https://github.com/AdrienGuille/GrowingNeuralGas"
+                sim.append_modeles(m)
 
     sim.init_metrics(False)
 
